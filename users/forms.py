72292c35266ef7140bd332940b8fdd129670e0b9
rsh <emkayhrushikesh@gmail.com>
2024-06-11 22:12:36 +0100

    Added extra fields to registration form with error handling

---
+++ users/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+import datetime
+
+class CustomUserCreationForm(UserCreationForm):
+    dob = forms.DateField(input_formats=['%d/%m/%Y'], widget=forms.DateInput(format='%d/%m/%Y'))
+    invitation_number = forms.CharField(max_length=6, min_length=6)
+
+    def clean_dob(self):
+        dob = self.cleaned_data['dob']
+        if (dob.year + 16) > datetime.date.today().year:
+            raise forms.ValidationError('You must be at least 16 to register')
+        return dob
--- users/views.py
+++ users/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Accoount created for {username}')
+            messages.success(request, f'Account created for {username}')
             return redirect('main-home')
+        else:
+            messages.error(request, 'Invalid form data')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
