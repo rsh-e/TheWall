c12966588da43941ec01e88490fbca48a7456bed
rsh <emkayhrushikesh@gmail.com>
2024-06-12 19:42:10 +0100

    Added an email field to the login page

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,11 @@
+# users/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 import datetime
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
     dob = forms.DateField(input_formats=['%d/%m/%Y'], widget=forms.DateInput(format='%d/%m/%Y'))
     invitation_number = forms.CharField(max_length=6, min_length=6)
 
@@ -12,3 +14,7 @@ class CustomUserCreationForm(UserCreationForm):
         if (dob.year + 16) > datetime.date.today().year:
             raise forms.ValidationError('You must be at least 16 to register')
         return dob
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'dob', 'invitation_number']
